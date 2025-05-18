cffbff988bfa87a68bc7d2f19c871d25f179c096
hariwebchirpy <hari@webchirpy.com>
2025-05-18 10:32:54 +0530

    Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
